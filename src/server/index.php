853e98f8a0fbfaa5bf6195a4a56ebd8d307ce104
Paul Binet <paul.binet1@etu.univ-lorraine.fr>
2017-11-28 11:51:05 +0100

    Adding ajax

--- src/server/index.php
+++ src/server/index.php
@@ -6,10 +6,10 @@ use Psr\Http\Message\ResponseInterface;
 
 $app = new \Slim\App();
 $app->add(function(ServerRequestInterface $request, ResponseInterface $response, callable $next){
-	$response = $response->withHeader('Content-type', 'application/json; charset=utf-8');
-	$response = $response->withHeader('Access-Control-Allow-Origin', '*');
-	$response = $response->withHeader('Access-Control-Allow-Methods', 'OPTION, GET, POST, PUT, PATCH, DELETE');
-	return $next($request, $response);
+    $response = $response->withHeader('Content-type', 'application/json; charset=utf-8');
+    $response = $response->withHeader('Access-Control-Allow-Origin', '*');
+    $response = $response->withHeader('Access-Control-Allow-Methods', 'OPTION, GET, POST, PUT, PATCH, DELETE');
+    return $next($request, $response);
 });
 
 $tasks_path = realpath('..') . '/database/tasks.json';
@@ -26,6 +26,21 @@ $app->group('/tasks', function() use($app, $tasks_path, $tasks) {
 
     $app->post('/addtask', function(ServerRequestInterface $request) use($tasks_path, $tasks) {
 
+        $taskName = $request->getParam('name');
+        $taskDescription = $request->getParam('description');
+        $taskDuration = $request->getParam('duration');
+        if(!empty($taskName) && !empty($taskDuration)){
+            if(file_exists($tasks_path)){
+                $current = json_decode(file_get_contents($tasks_path), true);
+            }
+            $current[]['name'] = $taskName;
+            $current[]['description'] = $taskDescription;
+            $current[]['duration'] = $taskDuration;
+
+            $to_json = json_encode($current);
+            file_put_contents($tasks_path, $to_json);
+            echo $to_json;
+        }
     });
 });
 
